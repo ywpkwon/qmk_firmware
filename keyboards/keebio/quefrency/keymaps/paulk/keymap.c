aa8034b81235ff5d44be470f40fb16f8ba4cf236
YW Paul Kwon <young@berkeley.edu>
2019-08-04 23:37:11 -0700

    quefrency keymap edit

--- keyboards/keebio/quefrency/keymaps/paulk/keymap.c
+++ keyboards/keebio/quefrency/keymaps/paulk/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ├─────────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┐  └──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴───────┬─────┤
    * │LSHFT       │  Z  │  X  │  C  │  V  │  B  │     │  N  │  M  │  ,  │  .  │  /  │RSHFT     │ UP  │
    * ├──────┬─────┴─┬───┴──┬──┴─────┴────┬┴─────┤     ├─────┴┬────┴────┬┴────┬┴────┬┴────┬─────┼─────┤
-   * │LCTRL │L_GUI  │L_ALT │FN1          │ SPC  │     │SPC   │BSPC     │R_ALT│R_CTR│LEFT │DOWN │RIGHT│
+   * │LCTRL │L_GUI  │L_ALT │FN1          │ SPC  │     │BSPC  │         │LEFT │DOWN │ UP  │     │RIGHT│
    * └──────┴───────┴──────┴─────────────┴──────┘     └──────┴─────────┴─────┴─────┴─────┴─────┴─────┘
    */
 
@@ -33,14 +33,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
     CTL_T(KC_ESC), LT(_FN1, KC_A),    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,  LT(_FN1, KC_SCLN), KC_QUOT, CTL_T(KC_ENT), \
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN1), \
-    CTL_T(KC_ESC),  KC_LGUI, KC_LALT, MO(_FN1), KC_SPC,          MO(_FN1),  KC_BSPC, KC_RALT, KC_LGUI,  KC_3, _______, CTL_T(KC_ENT)
+    CTL_T(KC_ENT),  KC_LGUI, MO(_FN1), KC_LALT, KC_SPC,          KC_BSPC,  _______, KC_LEFT, KC_DOWN,  KC_UP, _______, KC_RGHT \
   ),
 
+    /*
+   * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐     ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+   * │  `  │  F1 │ F2  │ F3  │ F4  │ F5  │ F6  │     │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │    DEL    │
+   * ├─────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┘  ┌──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴─────┤
+   * │TAB     │  Q  │  W  │  E  │  R  │  T  │     │  Y  │  U  │  I  │  O  │  P  │  [  │  ]  │  BSLS  │
+   * ├────────┴┬────┴┬────┴┬────┴┬────┴┬────┴┐    └┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴────────┤
+   * │LCTRL:ESC│  A  │  S  │  D  │  F  │  G  │     │  H  │  J  │  K  │  L  │  ;  │  '  │ENTER        │
+   * ├─────────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┐  └──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴───────┬─────┤
+   * │LSHFT       │  Z  │  X  │  C  │  V  │  B  │     │  N  │  M  │  ,  │  .  │  /  │RSHFT     │ UP  │
+   * ├──────┬─────┴─┬───┴──┬──┴─────┴────┬┴─────┤     ├─────┴┬────┴────┬┴────┬┴────┬┴────┬─────┼─────┤
+   * │LCTRL │L_GUI  │L_ALT │FN1          │ SPC  │     │DEL   │         │HOME │PGDN │PGUP │     │ END │
+   * └──────┴───────┴──────┴─────────────┴──────┘     └──────┴─────────┴─────┴─────┴─────┴─────┴─────┘
+   */
   [_FN1] = LAYOUT(
     KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL, \
     _______,  _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, _______, _______, _______, \
     _______,  _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, \
     _______,  RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, \
-    _______,  RGB_TOG, RGB_MODE_FORWARD, _______, _______,          _______, KC_DEL, _______, _______, _______, _______, _______
+    RGB_TOG, RGB_MODE_FORWARD, _______, KC_RALT, _______,          KC_DEL, _______, KC_HOME, KC_PGDN, KC_PGUP, _______, KC_END \
   )
 };
